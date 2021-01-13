cb6f402f1026e21dc86e97fc53dbdd24aad3a5fd
Taehwan Kim <freewebwithme777@protonmail.com>
2021-01-13 10:38:04 -0800

    set elixir, erlang and node version for deploy

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,9 @@ database_url =
 config :jaang, Jaang.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  # pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  # for Gigalixir free tier
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
